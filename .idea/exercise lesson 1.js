d68138a7c796d3be7c80a2d895032bcbc8045a0a
Tim Lagier <tim.lagier33@gmail.com>
2023-06-15 15:17:21 +0200

    Lesson 1

---
+++ .idea/exercise lesson 1.js
@@ -0,0 +1,9 @@
+alert('Good morning Good afternoon');
+
+alert('My name is Tim');
+
+10+5
+20-5
+2+2-5
+
+document.body.innerHTML='IM trying my best here'
---
+++ .idea/index.js
@@ -0,0 +1,9 @@
+alert('hello');
+alert('Good afternoon');
+alert('ur got urself a virus');
+
+2+2
+16*23
+
+document.body.innerHTML ='hello';
+
