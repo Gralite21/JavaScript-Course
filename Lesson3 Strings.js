c79cd048909f0ba779cd5db5ea0ff97924d2d522
Tim Lagier <tim.lagier33@gmail.com>
2023-06-17 16:06:41 +0200

    update

---
+++ Lesson3 Strings.js
@@ -0,0 +1,21 @@
+'hello';
+'some'+'text';
+typeof 2;
+typeof 'hello';
+'hello'+3;
+'$20';
+'$'+(2095+799)/100;
+'Items('+(1+1)+'):$'+(2095+799)/100;
+alert('Items('+(1+1)+'):$'+(2095+799)/100);
+"I'm";
+'I\'m';
+alert('some\ntext');
+`hello`;
+`Items(${1+1}):$${(2095+799)/100}`;
+`some
+text`;
+
+"Tim says:\"Hello\"";
+
+console.log(`some
+text`)
