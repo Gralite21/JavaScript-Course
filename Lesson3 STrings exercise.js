7a41bfb2e08a13a70a3e21b3d6326ba850f1f397
Tim Lagier <tim.lagier33@gmail.com>
2023-06-17 17:22:02 +0200

    update

---
+++ Lesson3 STrings exercise.js
@@ -0,0 +1,29 @@
+'My name is';
+
+'Tim';
+
+'My name is:"Tim"';
+
+`Total cost:$${5+3}`;
+
+'Total cost:$'+5+3;
+
+alert('Total cost:$'+5+3);
+
+alert(`Total cost:$${5+3}`);
+
+'Total cost:$'+(599+295)/100;
+
+`Total cost:$${(599+295)/100}`;
+
+alert(`Total cost:$${(599+295)/100}`);
+
+alert(`Total cost:$${(599+295)/100}
+Thank you come again`);
+
+`Items(${2+2}):$${(2*2095+2*799+2*499)/100}`;
+
+`Shipping & handling:$${(2*499)/100}`;
+
+`Total before tax:$${(2*2095+2*799+2*499)/100}`;
+
